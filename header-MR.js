d87a2161573b68b18a8b90fcc353ee941b12e0cd
Austine <chucky891213@naver.com>
2024-07-11 09:42:33 +0900

    24-07-11 MR header 스크립트 구현

--- header-MR.js
+++ header-MR.js
@@ -1,41 +1,42 @@
-// const navBar1 = document.querySelector(".tn1");
-// const navBar2 = document.querySelector(".tn2");
-// const navBar3 = document.querySelector(".tn3");
-// const navBar4 = document.querySelector(".tn4");
+const gnbs = document.querySelectorAll("#gnb > li");
 
-// console.log(navBar);
+const gnbThird = gnbs[2];
+const gnbFourth = gnbs[3];
+const gnbFifth = gnbs[4];
+const gnbSixth = gnbs[5];
 
-// function showToggleNavBar() {
-//   navBar.classList.toggle("global-toggle");
-// }
+const navLists = document.querySelectorAll("#gnb > div");
 
-// navBar1.addEventListener("mouseenter", () => showToggleNavBar(navBar1));
-// navBar2.addEventListener("mouseenter", () => showToggleNavBar(navBar2));
-// navBar3.addEventListener("mouseenter", () => showToggleNavBar(navBar3));
-// navBar4.addEventListener("mouseenter", () => showToggleNavBar(navBar4));
+const navBath = navLists[0];
+const navUtensils = navLists[1];
+const navLiving = navLists[2];
+const navCs = navLists[3];
 
-// navBar1.addEventListener("mouseleave", () => showToggleNavBar(navBar1));
-// navBar2.addEventListener("mouseleave", () => showToggleNavBar(navBar2));
-// navBar3.addEventListener("mouseleave", () => showToggleNavBar(navBar3));
-// navBar4.addEventListener("mouseleave", () => showToggleNavBar(navBar4));
+function show(nav) {
+  nav.classList.toggle("global-toggle");
+}
 
-// $(function () {
-//   for (let i = 1; i <= 4; i++) {
-//     $(".tn" + i).on("mouseenter", function () {
-//       $(".tn" + i).css({ display: "block" });
-//     });
-//   }
-// });
-
-$(function () {
-  for (let i = 1; i <= 4; i++) {
-    $(".tn" + i).on({
-      mouseenter: function () {
-        $(this).stop().fadeIn(200);
-      },
-      mouseleave: function () {
-        $(this).stop().fadeOut(200);
-      },
-    });
-  }
+gnbThird.addEventListener("mouseenter", function () {
+  show(navBath);
+});
+gnbThird.addEventListener("mouseleave", function () {
+  show(navBath);
+});
+gnbFourth.addEventListener("mouseenter", function () {
+  show(navUtensils);
+});
+gnbFourth.addEventListener("mouseleave", function () {
+  show(navUtensils);
+});
+gnbFifth.addEventListener("mouseenter", function () {
+  show(navLiving);
+});
+gnbFifth.addEventListener("mouseleave", function () {
+  show(navLiving);
+});
+gnbSixth.addEventListener("mouseenter", function () {
+  show(navCs);
+});
+gnbSixth.addEventListener("mouseleave", function () {
+  show(navCs);
 });
